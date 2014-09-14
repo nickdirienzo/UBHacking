f3b8e76d3700ef0959211af42747989dcf570a20
Alexander Rookey <rookierookey@gmail.com>
2014-09-14 00:32:28 -0400

    DONE

--- main.py
+++ main.py
@@ -28,42 +28,62 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def processFormData(self):
+  hacker = Hacker()
+  hacker.first_name = self.request.get('firstname')
+  hacker.last_name = self.request.get('lastname')
+  hacker.email = self.request.get('email')
+  hacker.school = self.request.get('school')
+  hacker.major = self.request.get('major')
+  hacker.github = self.request.get('github')
+  if self.request.get('firstHackathon'):
+    hacker.first_hackathon = True
+  if self.request.get('vegetarian'):
+    hacker.vegetarian  = True
+  hacker.dietary_restrictions = self.request.get('dietaryRestrictions')
+  hacker.shirt_size = self.request.get('shirtsize')
+  hacker.resume = self.request.get('resume')
+  hacker.put()
+  return True
+
 
 class Hacker(ndb.Model):
   first_name=ndb.StringProperty()
   last_name=ndb.StringProperty()
   email=ndb.StringProperty()
   school=ndb.StringProperty()
-  year=ndb.StringProperty()
   major=ndb.StringProperty()
   github=ndb.StringProperty()
-  firstHackathon=ndb.BooleanProperty()
-  dietaryRestrictions=ndb.StringProperty()
-  shirtSize=ndb.StringProperty()
+  first_hackathon=ndb.BooleanProperty()
+  vegetarian=ndb.BooleanProperty()
+  dietary_restrictions=ndb.StringProperty()
+  shirt_size=ndb.StringProperty()
   resume=ndb.BlobProperty()
   date=ndb.DateTimeProperty(auto_now_add=True)
 
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
-    template = JINJA_ENVIRONMENT.get_template('index.html')
+    template = JINJA_ENVIRONMENT.get_template('/template/home.html')
     self.response.write(template.render())
 
 
 class RegistrationHandler(webapp2.RequestHandler):
   def get(self):
-    template = JINJA_ENVIRONMENT.get_template('register/index.html')
+    template = JINJA_ENVIRONMENT.get_template('/template/registration.html')
     self.response.write(template.render())
 
 
 class SubmitHandler(webapp2.RequestHandler):
   def post(self):
-    self.redirect('/success')
-
+    if processFormData(self):
+      self.redirect("/success")
+    else:
+      self.response.write('uhhh...')
 
 class SuccessHandler(webapp2.RequestHandler):
   def get(self):
-    template = JINJA_ENVIRONMENT.get_template('success/index.html')
+    template = JINJA_ENVIRONMENT.get_template('/template/success.html')
     self.response.write(template.render())
 
 
